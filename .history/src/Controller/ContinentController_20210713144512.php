8d8949ca68928d5d5005c60254a83a671d20ea10
Massine <massinoueslati@gmail.com156>
2021-07-13 15:56:56 +0200

    Lister les animaux d'un seul continent + faire les liens entre les pages. Pour ne pas citer une deuxième fois le nom du continent de la page dans la liste des contienents où vit l'animal, j'ai fait un test if.

---
+++ .history/src/Controller/ContinentController_20210713144512.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContinentRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContinentController extends AbstractController
+{
+    /**
+     * @Route("/continents", name="continents")
+     */
+    public function index(ContinentRepository $repository): Response
+    {
+        $continents = $repository -> findAll();
+        return $this->render('continent/continents.html.twig', [
+            'continents' => $continents
+        ]);
+    }
+    /**
+     * @Route("/continent/{id}", name="afficher_continent")
+     */
+    public function index(Continent $repository): Response
+    {
+        $continents = $repository -> findAll();
+        return $this->render('continent/continents.html.twig', [
+            'continents' => $continents
+        ]);
+    }
+}
