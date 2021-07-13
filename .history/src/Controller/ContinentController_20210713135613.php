43cdfd4da7923ea4e2eda0c595a10d16c1bf01a8
Massine <massinoueslati@gmail.com156>
2021-07-13 14:26:48 +0200

    faire la liste de tous les continents : créer ContinentController, y faire la route vers la vue et créer cette vue (continents.html.twig) + ajouter lien Continents dans la navbar + faire quelques liens

---
+++ .history/src/Controller/ContinentController_20210713135613.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContinentRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContinentController extends AbstractController
+{
+    /**
+     * @Route("/continents", name="continents")
+     */
+    public function index(ContinentRepository $repository): Response
+    {
+        return $this->render('continent/continents.html.twig', [
+            'controller_name' => 'ContinentController',
+        ]);
+    }
+}
