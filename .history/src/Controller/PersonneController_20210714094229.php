a81bb5fe461c1484ae09ffba1a0a2ef7b0477df3
Massine <massinoueslati@gmail.com156>
2021-07-14 10:48:00 +0200

    1- Ajout de laliste des personnes possédant l'animal dans la page liste des animaux 2- Lister les personnes (création d'un controlleur plus sa vue 3- création d'une autre route pour l'affichage d'une personne 4- réaliser les différents liens pour la navigabilité entre les pages

---
+++ .history/src/Controller/PersonneController_20210714094229.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonneController extends AbstractController
+{
+    /**
+     * @Route("/personnes", name="personnes")
+     */
+    public function index(PersonneRepository $repository): Response
+    {
+        
+        return $this->render('personne/personnes.html.twig', [
+            'personnes' => $personnes
+        ]);
+    }
+}
